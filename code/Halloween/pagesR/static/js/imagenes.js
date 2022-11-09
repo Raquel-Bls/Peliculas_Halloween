bce741842bef9546531d3ca523c41af6cfafaac7
Raquel <raquelmtz224@gmail.com>
2022-11-08 22:44:32 -0600

    Se agregaron archivos estaticos

---
+++ code/Halloween/pagesR/static/js/imagenes.js
@@ -0,0 +1,40 @@
+function retornarFecha() {
+    var fecha
+    fecha = new Date();
+    var cadena = fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getYear();
+    return cadena;
+}
+
+function retornarHora() {
+    var fecha
+    fecha = new Date();
+    var cadena = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+    return cadena;
+}
+
+var photos = new Array()
+var which = 0
+
+/*Change the below variables to reference your own images. You may have as many images in the slider as you wish*/
+photos[0] = "/static/images/1.jpg"
+photos[1] = "/static/images/2.jpg"
+photos[2] = "/static/images/3.jpg"
+photos[3] = "/static/images/4.jpg"
+photos[4] = "/static/images/5.jpg"
+
+
+function backward() {
+    if (which > 0) {
+        window.status = ''
+        which--
+        document.images.photoslider.src = photos[which]
+    }
+}
+
+function forward() {
+    if (which < photos.length - 1) {
+        which++
+        document.images.photoslider.src = photos[which]
+    }
+    else window.status = 'End of gallery'
+}
